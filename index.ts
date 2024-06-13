66078fdfb9f89cd5cfebf5913a9d386055df1817
HFV <hector.kaizen@gmail.com>
2024-06-13 11:45:03 -0600

    [add] typescript

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
-const express = require('express')
+import express, {Request, Response} from 'express'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+
 // Rutas de ejemplo inicial
-app.get('/', (req, res) => res.send('Pokemon app'))
+app.get('/', (req: Request, res: Response) => res.send('Pokemon app'))
 
 // Iniciar el servidor
 app.listen(PORT, () => console.log(`SERVER ON => http://localhost:${PORT}`))
